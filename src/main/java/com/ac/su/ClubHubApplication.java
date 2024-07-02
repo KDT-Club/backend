fb0ab6362c0f0ac73db039c608f9ea96726239c3
이정훈 <niko36@naver.com>
2024-07-02 20:49:35 +0900

    git test for clubHub

---
+++ src/main/java/com/ac/su/ClubHubApplication.java
@@ -0,0 +1,13 @@
+package com.ac.su;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubHubApplication.class, args);
+    }
+
+}
