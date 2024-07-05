5bb599dfc09455187bb8786c5eb960b8a6553325
이정훈 <niko36@naver.com>
2024-07-05 15:26:52 +0900

    com.ac.su에 msg 전달을 위한 ResponseMessage 클래스 추가함

--- src/main/java/com/ac/su/SecurityConfig.java
+++ src/main/java/com/ac/su/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.ac.su;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig { //여기서 Spring Security 설정함
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { //어떤 페이지를 검사할지 설정 가능
+        http.csrf((csrf) -> csrf.disable()); //CSRF 보안 기능 끔
+        http.authorizeHttpRequests((authorize) ->
+                authorize.requestMatchers("/**").permitAll() ////모든 페이지 접속 허용
+        );
+        return http.build();
+    }
 }
+
+
+// FilterChain : 모든 유저의 요청과 서버의 응답 사이에 자동으로 실행해주고 싶은 코드 담는 곳
--- src/main/java/com/ac/su/member/MemberController.java
+++ src/main/java/com/ac/su/member/MemberController.java
@@ -1,4 +1,29 @@
 package com.ac.su.member;
 
+import com.ac.su.ResponseMessage;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberRepository memberRepository;
+    //메인페이지로 이동
+    @GetMapping("/")
+    ResponseMessage test() {
+        return new ResponseMessage("성공");
+    }
+    //회원가입 기능 - /signup
+    @PostMapping("/signup")
+    String signup() {
+        return "회원가입 성공"; //로그인 페이지로 이동 리다이렉트
+    }
 }
+
+
+
