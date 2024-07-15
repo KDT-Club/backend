fed078635c77eab2cdd316dab6e081cf7fef1e3f
dongjoonseo <ddoonngg2@naver.com>
2024-07-15 14:10:08 +0900

    24.07.15 서비스 탈퇴시 연관 테이터 삭제, 동아리 회장은 서비스 탈퇴 불가

--- src/main/java/com/ac/su/member/Member.java
+++ src/main/java/com/ac/su/member/Member.java
@@ -1,8 +1,16 @@
 package com.ac.su.member;
 
-import jakarta.persistence.*;
+import com.ac.su.clubmember.ClubMember;
+import com.ac.su.comment.Comment;
+import com.ac.su.community.club.Club;
+import com.ac.su.community.post.Post;
+import com.ac.su.joinrequest.JoinRequest;
+import jakarta.persistence.*;;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -32,6 +40,25 @@ public class Member {
 
     @Column
     private String phone;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Club> clubs = new ArrayList<>();
+
+    // 클럽 멤버와의 관계 설정 (cascade 옵션 추가)
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ClubMember> clubMembers = new ArrayList<>();
+
+    // 가입 요청과의 관계 설정 (cascade 옵션 추가)
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<JoinRequest> joinRequests = new ArrayList<>();
+
+    // 댓글과의 관계 설정 (cascade 옵션 추가)
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> Comment = new ArrayList<>();
+
+    // 게시글과의 관계 설정 (cascade 옵션 추가)
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> Post = new ArrayList<>();
 
     @Override
     public String toString() {
